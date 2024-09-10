bdcc3334810aee725101ea403c0cfb89bf9d449b
Justin St-Amant <jstamant24@gmail.com>
2024-09-10 09:22:50 -0500

    More incremental changes, added axum and sqlx

--- README.md
+++ README.md
@@ -12,18 +12,20 @@ There are no features implemented as of yet.
 
 Here's a general list of features I'd like to implement in this platform, roughly ordered by priority.
 
+### Initial release
 - Sign-up
 - Password login
-- Email code login
 - Create character sheets
-- Create templates
 - Create games/tables
+
+### Next release
+- Create templates
+- Email code login
+- In-game dice roller
 - Invite/add users to tables
 - Link sharing for tables - no sign-up required for players
-- In-game dice roller
 
-Low priority:
-
+### Future releases
 - Character sheet export to PDF
 - Payment and/or tipping
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from 'next/link';
 
 export default function Home() {
@@ -17,13 +16,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
           Learn
         </a>
         <a
@@ -32,13 +24,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
           Examples
         </a>
         <a
@@ -47,14 +32,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          Go to nextjs.org
         </a>
       </footer>
     </div>
