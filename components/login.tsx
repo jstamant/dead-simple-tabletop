c4f454d18780c113025907914ded235bcb197753
Justin St-Amant <jstamant24@gmail.com>
2024-09-25 10:18:29 -0500

    Added state to login

--- components/login.tsx
+++ components/login.tsx
@@ -1,4 +1,5 @@
 'use client'
+import {useState} from 'react'
 
 import {Button} from "@nextui-org/button";
 import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/card";
@@ -9,24 +10,35 @@ import {PressEvent} from '@react-types/shared'
 
 import axios from 'axios'
 
+// TODO this can be moved to a util/react.tsx library, and the generic can be changed from string to S??
+// TODO add documentation
+// Generic utility for updating state in a component's onChange attribute
+const setField = (setState: React.Dispatch<React.SetStateAction<string>>) => {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+        setState(event.target.value);
+    };
+}
+
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const handleCreateAccount = (e: PressEvent) => {
         console.log(e)
         console.log('creating user account clicked!')
-        axios.post('http://localhost:3001/user', {username: "testadmin", password: "test"});
+        axios.post('http://localhost:3001/user', {username: email, password: password});
     }
     const handleLogin = (e: PressEvent) => {
         console.log(e)
         console.log('login button clicked!')
-        axios.post('http://localhost:3001/login', {username: "testadmin", password: "test"});
+        axios.post('http://localhost:3001/login', {username: email, password: password});
     }
     return (
         <Card className="w-96">
             <CardHeader>Login</CardHeader>
             <Divider />
             <CardBody>This is a login box!</CardBody>
-            <Input label="Email" size="sm" />
-            <Input label="Password" size="sm" type="password" />
+            <Input label="Email" onChange={setField(setEmail)} size="sm" />
+            <Input label="Password" onChange={setField(setPassword)} size="sm" type="password" />
             <Divider />
             <CardFooter className="flex justify-center">
                 <Button className="w-1/2" onPress={handleLogin}>Login</Button>
