0c11029a198285a6faba157a7a1c00bada0458e4
Justin St-Amant <jstamant24@gmail.com>
2024-09-28 09:57:20 -0500

    Added editable state for sheets, added mdi icons

--- app/sheet/[id]/page.tsx
+++ app/sheet/[id]/page.tsx
@@ -1,7 +1,13 @@
 'use client'
+
 import {useEffect, useState} from 'react'
 
 import {Card, CardHeader, CardBody, CardFooter} from '@nextui-org/card'
+import {Divider} from '@nextui-org/divider'
+import {Button} from '@nextui-org/button'
+
+import Icon from '@mdi/react'
+import { mdiDrag } from '@mdi/js'
 
 import axios from '../../../util/axios'
 
@@ -15,19 +21,34 @@ interface Sheet {
 
 export default function SheetPage({ params }: { params: { id: number } }) {
     const [sheet, setSheet] = useState<Sheet>({id: params.id, user_id: 1, title: ''});
+    const [isEditing, setIsEditing] = useState(false);
 
     useEffect(() => {
         axios.get(`/sheets/${sheet.id}`).then((res) => setSheet(res.data));
     }, []);
 
+    const dragIcon = <Icon path={mdiDrag} size={1} className="inline" />;
+
     return (
-        <Card className="w-1/2 m-4">
-            <CardHeader>title: {sheet.title}</CardHeader>
-            <CardBody>Content</CardBody>
-            <CardFooter>
-                <p>My sheet id: {sheet.id}</p>
-                <p>user_id: {sheet.user_id}</p>
-            </CardFooter>
-        </Card>
+        <div className="flex m-4">
+            <Card className="w-1/2">
+                <CardHeader>title: {sheet.title}</CardHeader>
+                <Divider />
+                <CardBody>
+                    <div>{isEditing ? dragIcon : ''} one field</div>
+                    <div>{isEditing ? dragIcon : ''} one field</div>
+                    <div>{isEditing ? dragIcon : ''} one field</div>
+                    <div>{isEditing ? dragIcon : ''} one field</div>
+                    <div>{isEditing ? dragIcon : ''} one field</div>
+                    <div>{isEditing ? dragIcon : ''} one field</div>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                    <p>My sheet id: {sheet.id}</p>
+                    <p>user_id: {sheet.user_id}</p>
+                </CardFooter>
+            </Card>
+            <Button onPress={() => setIsEditing(!isEditing)}>Edit this sheet</Button>
+        </div>
     )
 }
